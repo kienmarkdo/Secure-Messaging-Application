6f83a5a4788ec7fe04407d71b4aa9e97b310d6a0
philipostr <philipostroscki@yahoo.ca>
2023-03-04 00:18:33 -0500

    Added auth connection

--- src/FirebaseConnect.js
+++ src/FirebaseConnect.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
@@ -15,3 +16,5 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Realtime Database and get a reference to the service
 const database = getDatabase(app);
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
