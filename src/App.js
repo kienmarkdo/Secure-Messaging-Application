e3998e04587d20ee96794775ee7f48d9200fb6e9
Kien Do <kienmarkdo@gmail.com>
2023-03-04 01:07:07 -0500

    Added button and dark background

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import "./App.css";
+import { Button, Input } from "antd";
+import { useState } from "react";
 
 function App() {
+  const [loadingStatus, setLoadingStatus] = useState(false);
+
+  const enterLoading = () => {
+    setLoadingStatus(true);
+
+    setTimeout(() => setLoadingStatus(false), 2000);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div>
+      <div className="generate-chat-container">
+        <div></div>
+        <Button
+          shape="round"
+          loading={loadingStatus}
+          type="primary"
+          onClick={enterLoading}
+          style={{ height: "100%", fontSize: "30px" }}
+        >
+          Generate New Chat!
+        </Button>
+      </div>
     </div>
   );
 }
